d72552b4575a292d9541307e5cbd5f124677ca3e
DuJuan Pugh <dujuanpugh185@gmail.com>
2025-03-25 08:50:07 -0400

    Initial Commit

---
+++ we are cookin/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  we are cookin
+//
+//  Created by DUJUAN PUGH on 3/25/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ we are cookin/we_are_cookinApp.swift
@@ -0,0 +1,17 @@
+//
+//  we_are_cookinApp.swift
+//  we are cookin
+//
+//  Created by DUJUAN PUGH on 3/25/25.
+//
+
+import SwiftUI
+
+@main
+struct we_are_cookinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
